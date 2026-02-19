6671bb9d118ec3bae56164ae583430e091fa308c
Samuel <samuel.epl@hotmail.com>
2026-02-19 09:38:46 -0400

    Create command line interface

---
+++ src/expensetracker/ui/cli.py
@@ -0,0 +1,38 @@
+import argparse 
+
+
+def run_cli():
+    #Create Parser
+    parser = argparse.ArgumentParser(description="Track your expenses!")
+
+    # Create subparser
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    # Create subcommands
+    add_parser = subparsers.add_parser("add")
+    update_parser = subparsers.add_parser("update")
+    delete_parser = subparsers.add_parser("delete")
+    list_parser = subparsers.add_parser("list")
+    summary_parser = subparsers.add_parser("summary")
+
+    # Create arguments for the "add" subcommand
+    add_parser.add_argument("--description", required=True)
+    add_parser.add_argument("--amount", type=int, required=True)
+
+    # Create arguments for the "update" subcommand
+    update_parser.add_argument("--id", type=int, required=True)
+
+    # Create arguments for the "delete" subcommand
+    delete_parser.add_argument("--id", type=int, required=True)
+
+    # Create arguments for the "summary" subcommand
+    summary_parser.add_argument("--month", type=int)
+
+    # Read and validate what the user wrote in the terminal
+    args = parser.parse_args()
+    
+    return args 
+   
+
+
+
